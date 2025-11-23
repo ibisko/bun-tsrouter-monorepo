666a30605386c7718ccfa6c5ebdac466764e268a
yinghang <1337285102@qq.com>
2025-11-24 06:51:15 +0800

    chore: .

--- dev.ts
+++ dev.ts
@@ -1,15 +1,12 @@
-const exec = (cwd: string) => Bun.spawn(["bun", "run", "dev"], {
+const exec = (cwd: string) =>
+  Bun.spawn(['bun', 'run', 'dev'], {
     cwd,
-    stdout: "inherit", // 输出到当前终端
-})
+    stdout: 'inherit', // 输出到当前终端
+  });
 
 // 等待所有任务完成（或任意一个失败）
 ~(async function () {
-    await Promise.all([
-        exec("./packages/utils"),
-        exec("./packages/ui"),
-        exec("./packages/tsrouter"),
-        exec("./apps/web"),
-        exec("./apps/server"),
-    ]);
-}())
+  Promise.all([exec('./packages/utils'), exec('./packages/ui'), exec('./packages/tsrouter')]);
+  await new Promise(r => setTimeout(r, 1e3));
+  await Promise.all([exec('./apps/web'), exec('./apps/server')]);
+})();
