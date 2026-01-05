8b18de1200689995d818f60d239ab1ed64def699
yinghang <1337285102@qq.com>
2026-01-05 14:30:30 +0800

    feat: package/tsrouter

--- packages/tsrouter/src-server/main.ts
+++ packages/tsrouter/src-server/main.ts
@@ -2,4 +2,3 @@ export * from './procedure';
 export * from './error';
 export * from './logger';
 export type { SseService, ReplaceSpecificLeaf, Context, WriteFunc, Middleware, UploadFileService } from './type';
-export { UploadMultipartCallback } from './multipart';
