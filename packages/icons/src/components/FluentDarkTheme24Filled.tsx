65541b3e13ee5fa1a837381a052a1bce8d315b89
yinghang <1337285102@qq.com>
2026-04-10 02:13:48 +0800

    feat: icons

---
+++ packages/icons/src/components/FluentDarkTheme24Filled.tsx
@@ -0,0 +1,10 @@
+import type { SVGProps } from 'react';
+
+/** fluent:dark-theme-24-filled */
+export function FluentDarkTheme24Filled(props: SVGProps<SVGSVGElement>) {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="1em" height="1em" {...props}>
+      <path fill="currentColor" d="M12 22c5.523 0 10-4.477 10-10S17.523 2 12 2S2 6.477 2 12s4.477 10 10 10m0-2V4a8 8 0 1 1 0 16" />
+    </svg>
+  );
+}
