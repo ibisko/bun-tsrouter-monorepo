7a918c2c601ed733b4cba69e6ef335ec12249f33
yinghang <1337285102@qq.com>
2026-06-15 15:52:24 +0800

    feat: packages/ui switch

--- packages/ui/src/main.ts
+++ packages/ui/src/main.ts
@@ -11,5 +11,6 @@ export * from './components/Dialog';
 export * from './components/Tooltip';
 export * from './components/Popover';
 export * from './components/Reveal';
+export * from './components/Switch';
 
 export * from './utils/cn';
