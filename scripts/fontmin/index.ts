0a3af399979586610128cf2bb146d1dda2746e01
yinghang <1337285102@qq.com>
2026-09-01 11:18:05 +0800

    chore: remove scripts/, move fontmin to packages/tools

--- scripts/fontmin/index.ts
+++
@@ -1,8 +0,0 @@
-import path from 'path';
-import { fontmin } from './core';
-
-fontmin({
-  folders: path.join(__dirname, '../..', 'apps/web/src'),
-  from: path.join(__dirname, '../..', 'packages/ui/fonts', 'LXGWWenKaiMono-Medium.ttf'),
-  to: path.join(__dirname, '../..', 'packages/ui/fonts', 'LXGWWenKaiMono-Medium.woff2'),
-});
